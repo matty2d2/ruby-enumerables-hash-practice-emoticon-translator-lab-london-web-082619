0af1dc23bbb4455b8dcdaadfd4786449ece3c929
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-22 12:53:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def load_library(path)
 end
 
 def get_japanese_emoticon
-  emoticon_hash = load_library(path)
+  emot_hash = load_library(path)
   result = emoticon_hash["get_emoticon"][emoticon]
   if result == nil
     result = "Sorry, that emoticon was not found" 
