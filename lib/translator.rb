ec2aeba85c4adc09c099d23c2c67dc8ba0b43ea5
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-22 12:51:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def load_library(path)
   emoticons = YAML.load_file(path)
-  emoticon_hash = Hash.new
+  emot_hash = Hash.new
 
   emoticon_hash["get_emoticon"] = Hash.new
   emoticon_hash["get_meaning"] = Hash.new
